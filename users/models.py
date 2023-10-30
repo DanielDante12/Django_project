6f0be96c6489db5b186ca68f588ae4bddd3d4839
DanielDante12 <ofwonodani12@gmail.com>
2023-10-30 14:18:50 +0300

    Customer model created

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
+from django.contrib.auth.models import User
+class Customer(models.Model):
+    firstname =models.CharField(max_length=20)
+    lastname=models.CharField(max_length=20)
+    dateOfBirth=models.DateField()
+    nationality=models.CharField(max_length=20)
+    district=models.CharField(max_length=20)
+    def __str__(self):
+        return self.firstname
 
 # Create your models here.
