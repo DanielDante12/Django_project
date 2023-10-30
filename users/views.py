3bbc0ed06f2ba63cd48e75c3d699566a7448ef45
DanielDante12 <ofwonodani12@gmail.com>
2023-10-30 14:20:13 +0300

    views.py modified

--- users/views.py
+++ users/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from django.contrib import messages
 from .forms import UserRegisterForm
+from .serializer import CustomerSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from .models import Customer
+@api_view(['GET','POST'])
+def home(request):
+    if request.method=="GET":
+          customers=Customer.objects.all()
+          SerializedCustomer=CustomerSerializer(customers, many=True)
+          return Response(SerializedCustomer.data)
+    elif request.method=='POST':
+        serialized=CustomerSerializer(data=request.data)  
+        if serialized.is_valid(): 
+            serialized.save()
+            return Response(serialized.data,status=status.HTTP_200_OK)
+
 def register(request):
     if request.method=='POST':
         form= UserRegisterForm(request.POST)
